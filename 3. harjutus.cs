dc62ca18461cc1bdb72e02f97fb68b3de4147fc6
Varavavaht <32923788+Varavavaht@users.noreply.github.com>
2017-10-30 01:38:10 +0200

    Add files via upload

---
+++ 3. harjutus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int magicarv = rnd.Next(1, 100); ;
+            Console.WriteLine("Arvuti valis välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata");
+            Console.Write("Sisesta number 1-st 100-ni: ");
+            string input1 = Console.ReadLine();
+            int arv = int.Parse(input1);
+            Console.WriteLine("Sa valisid arvu " + input1);
+            if (magicarv < arv)
+            {
+                Console.WriteLine("Sinu arv on liiga suur");
+            }
+            if (magicarv > arv)
+            {
+                Console.WriteLine("Sinu pakutud arv on liiga väike");
+            }
+            if (magicarv == arv)
+            {
+                Console.WriteLine("Sa valisid õige numbri!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
